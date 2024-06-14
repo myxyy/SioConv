b966bf23a6a8f9c26c2b3b157cf55818384293df
myxyy <m.myxomatosis@gmail.com>
2024-06-14 20:47:21 +0900

    検証用に整理

--- src/text_loader.py
+++ src/text_loader.py
@@ -8,10 +8,9 @@ import os
 import datasets
 
 class TextDataset(Dataset):
-    def __init__(self, path: str, size: int, tokenizer, transforms=None, tokenized_file_name=None) -> None:
+    def __init__(self, path: str, size: int, tokenizer, tokenized_file_name=None) -> None:
         super().__init__()
         self.size = size
-        self.transforms = transforms
 
         print('loading...')
 
@@ -28,25 +27,23 @@ class TextDataset(Dataset):
             chunk_list = []
             print('tokenizing...')
             for i in tqdm(range(len(text_raw)//chunk_size+1)):
-                chunk_list.append(tokenizer.encode(text_raw[i*chunk_size:(i+1)*chunk_size].lower()))
+                chunk_list.append(tokenizer.encode(text_raw[i*chunk_size:(i+1)*chunk_size].lower(), add_special_tokens=False))
             print('merging...')
-            self.text = np.array([token for chunk in chunk_list for token in chunk])
-            np.save(tokenized_file_name, self.text)
+            text = np.array([token for chunk in chunk_list for token in chunk])
+            np.save(tokenized_file_name, text)
         else:
-            self.text = tokenized_text
+            text = tokenized_text
+        text_num = (len(text)-1)//self.size
+        self.text_num = text_num
+        self.text = text[0:text_num * self.size].reshape(text_num, self.size)
+        self.text_next = text[1:text_num * self.size+1].reshape(text_num, self.size)
 
     def __getitem__(self, index: int) -> Tuple[torch.Tensor, torch.Tensor]:
-        data = self.text[index*self.size:(index+1)*self.size]
-        data_next = self.text[index*self.size+1:(index+1)*self.size+1]
-
-        if self.transforms is not None:
-            data = self.transforms(data)
-            data_next = self.transforms(data_next)
-        return data, data_next
+        return self.text[index], self.text_next[index]
 
 
     def __len__(self) -> int:
-        return (len(self.text)-1)//self.size
+        return self.text_num
 
 class HFDataset(Dataset):
     def __init__(self, dataset: datasets.arrow_dataset.Dataset, column: str, size: int, tokenizer, transforms=None, tokenized_file_name=None) -> None:
@@ -68,25 +65,22 @@ class HFDataset(Dataset):
             chunk_list = []
             print('tokenizing...')
             for i in tqdm(range(len(text_raw)//chunk_size+1)):
-                chunk_list.append(tokenizer.encode(text_raw[i*chunk_size:(i+1)*chunk_size].lower()))
+                chunk_list.append(tokenizer.encode(text_raw[i*chunk_size:(i+1)*chunk_size].lower(), add_special_tokens=False))
             print('merging...')
-            self.text = np.array([token for chunk in chunk_list for token in chunk])
-            np.save(tokenized_file_name, self.text)
+            text = np.array([token for chunk in chunk_list for token in chunk])
+            np.save(tokenized_file_name, text)
         else:
-            self.text = tokenized_text
+            text = tokenized_text
+        text_num = (len(text)-1)//self.size
+        self.text_num = text_num
+        self.text = text[0:text_num * self.size].reshape(text_num, self.size)
+        self.text_next = text[1:text_num * self.size+1].reshape(text_num, self.size)
 
     def __getitem__(self, index: int) -> Tuple[torch.Tensor, torch.Tensor]:
-        data = self.text[index*self.size:(index+1)*self.size]
-        data_next = self.text[index*self.size+1:(index+1)*self.size+1]
-
-        if self.transforms is not None:
-            data = self.transforms(data)
-            data_next = self.transforms(data_next)
-        return data, data_next
-
+        return self.text[index], self.text_next[index]
 
     def __len__(self) -> int:
-        return (len(self.text)-1)//self.size
+        return self.text_num
 
 class InterleaveHFDataset(Dataset):
     def __init__(self, dataset_list, column_list, tokenizer, size, cache_dir=None, seed=0):
