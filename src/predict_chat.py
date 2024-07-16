4a49473cc05f140ba8f202ecd3107f71e113c0e8
myxyy <m.myxomatosis@gmail.com>
2024-07-16 22:02:32 +0900

    チャット用スクリプト仮作成

---
+++ src/predict_chat.py
@@ -0,0 +1,108 @@
+import torchvision.transforms as transforms
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+import numpy as np
+import hydra
+from hydra.utils import instantiate
+from helper import calc_num_parameters
+
+np.set_printoptions(threshold=np.inf)
+
+@hydra.main(version_base=None, config_path="../configs/", config_name="config")
+def main(cfg):
+    devices = cfg.predict.devices
+    ckpt = torch.load(cfg.predict.weight)
+    tokenizer = instantiate(cfg.tokenizer)
+    model = instantiate(cfg.model)
+    vocab_size = tokenizer.vocab_size
+    model = model(devices=devices, vocab_size=vocab_size)
+    model.load_state_dict(ckpt['model'])
+    hidden_init = ckpt['hidden']
+    model.eval()
+    context_len = cfg.predict.context_len
+    out_length = cfg.predict.max_len
+    dtype = model.dtype
+    temperature = cfg.predict.temperature
+
+    del ckpt
+    torch.cuda.empty_cache()
+
+    num_parameters = calc_num_parameters(model)
+    print(f"#parameter:{num_parameters}")
+
+    model.reset_hidden()
+
+    def predict(prompt):
+        prompt = torch.from_numpy(np.array(tokenizer.encode(prompt)).astype(int)).clone().to(devices[0])
+        prompt_len = len(prompt)
+        input_len = len(tokenizer.decode(prompt))
+        prompt = torch.nn.functional.pad(prompt, (0, out_length-prompt_len), 'constant', 0)
+
+        beam_width = 1
+        #model.set_hidden(hidden_init)
+
+        current_len = 0
+        model.set_is_refresh(True)
+        prompt_beam = prompt.repeat(beam_width, 1)
+        while current_len <= prompt_len:
+            x = prompt_beam[:,current_len:current_len+context_len]
+            x = x.long()
+            if (prompt_len - current_len <= context_len):
+                model.set_is_refresh(prompt_len - current_len == context_len)
+                predict_init = model(x) # (1, context_len, vocab_size)
+                #predict_init_i = predict_init.view(context_len, vocab_size)[prompt_len - current_len -1].topk(beam_width)
+                predict_init_i = torch.multinomial(nn.Softmax(dim=1)(predict_init[:,prompt_len-current_len-1,:]/temperature), 1)
+                prompt_beam[:,prompt_len] = predict_init_i
+                current_len = prompt_len + 1
+            else:
+                model.set_is_refresh(True)
+                model(x)
+                current_len += context_len
+
+        out_last = input_len
+
+        while current_len < out_length:
+            model.set_is_refresh(current_len % context_len == 0)
+            complete_len = ((current_len-1) // context_len) * context_len
+            x = prompt_beam[:,complete_len:complete_len+context_len]
+            x = x.long()
+            predict_beam = model(x).to(devices[0])
+            predict_beam_i = torch.multinomial(nn.Softmax(dim=1)(predict_beam[:,current_len-complete_len-1,:]/temperature), 1)
+            prompt_beam[:,current_len] = predict_beam_i
+
+            current_len += 1
+            if predict_beam_i == tokenizer.bos_token_id:
+                break
+
+            predict = prompt_beam[0]
+            predict = predict.cpu().numpy()
+
+            chars = tokenizer.decode(predict[:current_len].tolist())
+            #print(predict[0:current_len+10])
+            #print(chars)
+            delimiter_ind = max(chars[out_last:].find(" "), chars[out_last:].find("\n")) + out_last
+            if delimiter_ind >= out_last:
+                print(chars[out_last:delimiter_ind+1], end='', flush=True)
+                out_last = delimiter_ind+1
+            elif len(chars[out_last:]) > 1:
+                print(chars[out_last:out_last+1], end='', flush=True)
+                out_last += 1
+
+        chars = tokenizer.decode(predict[:current_len-1].tolist(), clean_up_tokenization_spaces=True)[out_last:]
+        print(chars, end='', flush=True)
+
+
+        predict = prompt_beam[0]
+        predict = predict.cpu().numpy()
+        predict = tokenizer.decode(predict.tolist())
+        return predict
+
+    while True:
+        prompt = input('question:\n') + tokenizer.bos_token
+        print('\nanswer:')
+        predict(prompt)
+        print('\n')
+
+if __name__ == '__main__':
+    main()
