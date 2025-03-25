8c386e446dfe392a18f743afe408aca64b2fb3c0
myxyy <m.myxomatosis@gmail.com>
2025-03-26 00:54:26 +0900

    メモリが足らない

--- src/model/neural_memory.py
+++ src/model/neural_memory.py
@@ -4,6 +4,37 @@ import torch.nn.functional as F
 import numpy as np
 import einops
 
+class RMSNorm(nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-6, elementwise_affine=True, device=None):
+        super().__init__()
+        self.normalized_shape = dim
+        self.eps = eps
+        self.elementwise_affine = elementwise_affine
+        if self.elementwise_affine:
+            self.weight = nn.Parameter(torch.ones(dim, device=device))
+        else:
+            self.weight = None
+
+    def _norm(self, x):
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        output = self._norm(x.float()).type_as(x)
+        if self.weight is not None:
+            output = output * self.weight
+        return output
+
+class FFNSwiGLU(nn.Module):
+    def __init__(self, dim: int, dim_ff_hidden: float):
+        super().__init__()
+        self.fc = nn.Linear(dim, dim_ff_hidden)
+        self.fc_act = nn.Linear(dim, dim_ff_hidden)
+        self.fc_out = nn.Linear(dim_ff_hidden, dim)
+        self.act = nn.SiLU()
+    def forward(self, x):
+        x = self.fc(x) * self.act(self.fc_act(x))
+        x = self.fc_out(x)
+        return x
 
 def silu_backward(x):
     return F.silu(x) + F.sigmoid(x) * (1 - F.silu(x))
@@ -187,3 +218,120 @@ class ChunkwiseNeuralMemory(nn.Module):
 
     def set_hidden(self, hidden):
         self.last_hidden = hidden
+
+class NeuralMemoryBlock(nn.Module):
+    def __init__(self, dim: int, dim_ff_hidden: int, base_lr: float, dropout: float, chunk_size: int):
+        super().__init__()
+        self.memory = ChunkwiseNeuralMemory(dim, dim_ff_hidden, base_lr, chunk_size)
+        self.ffn = FFNSwiGLU(dim, dim_ff_hidden)
+        self.norm_memory = RMSNorm(dim)
+        self.norm_ffn = RMSNorm(dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        x_ = x
+        x = self.norm_memory(x)
+        x = self.memory(x)
+        x = self.dropout(x)
+        x = x + x_
+
+        x_ = x
+        x = self.norm_ffn(x)
+        x = self.ffn(x)
+        x = self.dropout(x)
+        x = x + x_
+
+        return x
+
+    def reset_hidden(self):
+        self.memory.reset_hidden()
+
+    def set_is_refresh(self, is_refresh):
+        self.memory.set_is_refresh(is_refresh)
+
+    def get_hidden(self):
+        return self.memory.get_hidden()
+
+    def set_hidden(self, hidden):
+        self.memory.set_hidden(hidden)
+
+class NeuralMemoryLM(nn.Module):
+    def __init__(
+        self,
+        depth: int,
+        dim: int,
+        dim_ff_hidden: int,
+        base_lr: float,
+        dropout: float,
+        vocab_size: int,
+        devices,
+        chunk_size: int=512,
+        token_in_out_parameter_corr = 3.0,
+        out_only_device: bool=True,
+    ):
+        super().__init__()
+        self.devices = devices
+        self.vocab_size = vocab_size
+        self.token_in = nn.Embedding(vocab_size, dim, device=devices[0], max_norm=1)
+        self.token_out = nn.Linear(dim, vocab_size, device=devices[-1])
+        self.block_list = nn.ModuleList([NeuralMemoryBlock(dim, dim_ff_hidden, base_lr, dropout, chunk_size) for _ in range(depth)])
+        self.norm_last = RMSNorm(dim, device=devices[-1])
+
+        self.token_in_out_parameter_corr = token_in_out_parameter_corr
+        self.num_parameters_token_in = sum(p.numel() for p in self.token_in.parameters())
+        self.num_parameters_per_block = sum(p.numel() for p in self.block_list[0].parameters())
+        self.num_parameters_norm_last = sum(p.numel() for p in self.norm_last.parameters())
+        self.num_parameters_token_out = sum(p.numel() for p in self.token_out.parameters())
+        self.num_parameters = (self.num_parameters_per_block * depth) + self.num_parameters_norm_last + (self.num_parameters_token_in + self.num_parameters_token_out)
+        self.out_only_device = out_only_device
+
+        for i, block in enumerate(self.block_list):
+            self.block_list[i] = block.to(devices[self.device_index(i)])
+
+    def device_index(self, i):
+        return (int)(((len(self.devices)-(1 if self.out_only_device else 0)) * (i * self.num_parameters_per_block + self.num_parameters_token_in)) / self.num_parameters)
+
+    def forward(self, x):
+        x = self.token_in(x)
+        for i, block in enumerate(self.block_list):
+            x = x.to(self.devices[self.device_index(i)])
+            x = block(x)
+        x = x.to(self.devices[-1])
+        x = self.norm_last(x)
+        x = self.token_out(x)
+        return x 
+
+    def reset_hidden(self):
+        for block in self.block_list:
+            block.reset_hidden()
+
+    def set_is_refresh(self, is_refresh):
+        for block in self.block_list:
+            block.set_is_refresh(is_refresh)
+
+    def get_hidden(self):
+        hidden_list = []
+        for block in self.block_list:
+            hidden = block.get_hidden()
+            if hidden is None:
+                return None
+            hidden_list.append(hidden.cpu())
+        return hidden_list
+
+    def set_hidden(self, hidden_list):
+        for i, block in enumerate(self.block_list):
+            block.set_hidden(hidden_list[i].to(self.devices[self.device_index(i)]) if hidden_list is not None else None)
+
+    def module_list(self):
+        blistlist = []
+        for _ in self.devices:
+            blistlist.append([])
+        for i, block in enumerate(self.block_list):
+            blistlist[self.device_index(i)].append(block)
+        mlist = []
+        for blist in blistlist:
+            mlist.append(nn.Sequential(*blist))
+        mlist[0] = nn.Sequential(self.token_in, mlist[0])
+        mlist[-1] = nn.Sequential(mlist[-1], self.norm_last, self.token_out)
+        return mlist
+        
